fe91b345180a1499bc62345a77b24707e4e30982
juan <jdcastano_1@uqvirtual.edu.co>
2020-09-15 00:05:16 -0500

    cambio imagen principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,7 +8,7 @@
 <aside>
         <!--publicidad-->
         <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fepampliega.com%2Fblog%2Findex.php%2F2010%2F10%2F17%2Fla-libreria-de-javier%2F&psig=AOvVaw2ohletf2Xe0peCojol4mWT&ust=1600232236941000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNi64eOv6usCFQAAAAAdAAAAABAD" 
+        src="https://epampliega.com/blog/wp-content/uploads/2018/09/libreriaJ.jpg" 
         alt="Imagen Peluqueria il éclat" title="Ver más información sobre Peluqueria il éclat"/>
 </aside>
 
