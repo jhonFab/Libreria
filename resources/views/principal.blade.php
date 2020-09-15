01bd8380aaf518de03031bc37351828eeb54bb45
juan <jdcastano_1@uqvirtual.edu.co>
2020-09-14 23:58:15 -0500

    cambio imagen en principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,7 +8,7 @@
 <aside>
         <!--publicidad-->
         <img
-        src="https://www.celuguia.com/inicio/media/large/12529_wp_20150317_11_54_07_pro.jpg" 
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fepampliega.com%2Fblog%2Findex.php%2F2010%2F10%2F17%2Fla-libreria-de-javier%2F&psig=AOvVaw2ohletf2Xe0peCojol4mWT&ust=1600232236941000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNi64eOv6usCFQAAAAAdAAAAABAD" 
         alt="Imagen Peluqueria il éclat" title="Ver más información sobre Peluqueria il éclat"/>
 </aside>
 
